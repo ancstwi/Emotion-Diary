391ad24ae345dfc40db35a9941aadd81d7ad0e71
milanychh <kudashevamilana@mail.ru>
2026-01-28 12:00:00 +0300

    feat (mobile): add base colors and layout constants

---
+++ mobile/src/constants/colors.js
@@ -0,0 +1,11 @@
+import { lightPalette } from '../theme/palettes';
+
+export const SCREEN_THEME = {
+  screenBg: lightPalette.screenBg,
+  fieldBg: lightPalette.fieldBg,
+  accent: lightPalette.accent,
+  text: lightPalette.text,
+  placeholder: lightPalette.placeholder,
+};
+
+export const COLORS = lightPalette;
